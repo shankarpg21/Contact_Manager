20093b6cb1364965fb1cbd60109a17c2e365640f
shankarpg21 <125481263+shankarpg21@users.noreply.github.com>
2023-07-06 11:33:22 +0530

    Update contactController.js

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,14 +2,14 @@ const Contact=require('../models/contactModel');
 const asyncHandler=require('express-async-handler');
 
 const getContacts=asyncHandler(async(req,res)=>{
-    const contacts=await Contact.find({user_id:req.user.id})
+    const contacts=await Contact.find({user_id:req.user_id})
     res.status(200).json(contacts)
 })
 
 const getContact=asyncHandler(async(req,res)=>{
     const reqContact=await Contact.findOne({"name":req.params.name});
     if(!reqContact){
-        res.status(400)
+        res.status(404)
         throw new Error("Contact not found")
     }
     res.status(200).json(reqContact)
@@ -31,16 +31,15 @@ const createContact=asyncHandler(async(req,res)=>{
 const updateContact=asyncHandler(async(req,res)=>{
     const reqContact=await Contact.findOne({"name":req.params.name})
     if(!reqContact){
-        res.status(400);
+        res.status(404);
         throw new error("Contact not found");
     }
     const {name,email,Phone}=req.body;
-
-    if(reqContact.user_id.toString()!=req.user_id){
+    if(reqContact.user_id!=req.user.id){
         res.status(403);
         throw new Error("No permission");
     }
-    await Contact.updateOne({"name":req.params.name},{$set:{"name":name,"email":email,"Phone":Phone}})
+    await Contact.updateOne({"name":name},{$set:{"name":name,"email":email,"Phone":Phone}})
     const newContact=await Contact.findOne({"name":req.params.name})
     res.status(200).json(newContact)
 })
@@ -48,14 +47,14 @@ const updateContact=asyncHandler(async(req,res)=>{
 const deleteContact=asyncHandler(async(req,res)=>{
     const reqContact=await Contact.findOne({"name":req.params.name})
     if(!reqContact){
-        res.status(400); 
-        throw new error("Contact not found");
+        res.status(404); 
+        throw new Error("Contact not found");
     }
-    if(reqContact.user_id.toString()!=req.user_id){
+    if(reqContact.user_id.toString()!=req.user.id){
         res.status(403);
         throw new Error("No permission");
     }
-    await Contact.findByIdAndRemove(req.params.name)
+    await Contact.deleteOne({"name":req.params.name})
     res.status(200).json("Contact Deleted")
 })
 
